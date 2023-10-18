91d4b43d9a63b6660bd58e6cf717560a468eb7af
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-10-17 18:50:57 -0700

    update

--- builtins.c
+++ builtins.c
@@ -2,18 +2,16 @@
 
 /**
  * print_env - prints the env variables
- * @p: pointer to env list
  */
 
-void print_env(env_t *p)
+void print_env(void)
 {
-	env_t *curr;
+	int i = 0;
 
-	curr = p;
-	while (curr)
+	while (environ[i])
 	{
-		write(STDOUT_FILENO, curr->str, ft_strlen(curr->str));
+		write(STDOUT_FILENO, environ[i], ft_strlen(environ[i]));
 		write(STDOUT_FILENO, "\n", 1);
-		curr = curr->next;
+		i++;
 	}
 }
